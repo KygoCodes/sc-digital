6a1cc4434768cf09f032f6efdaa5edf1028a9c82
KygoCodes <anododa@gmail.com>
2024-12-02 13:57:07 -0500

    Updated blog and article content

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,9 +1,37 @@
+/** @type {import('tailwindcss').Config} */
 module.exports = {
   content: [
     './src/**/*.{js,ts,jsx,tsx,mdx}',
   ],
   theme: {
-    extend: {},
+    extend: {
+      typography: {
+        DEFAULT: {
+          css: {
+            maxWidth: '100%',
+            color: 'rgb(var(--foreground-rgb))',
+            h1: {
+              color: 'rgb(var(--foreground-rgb))',
+            },
+            h2: {
+              color: 'rgb(var(--foreground-rgb))',
+            },
+            h3: {
+              color: 'rgb(var(--foreground-rgb))',
+            },
+            strong: {
+              color: 'rgb(var(--foreground-rgb))',
+            },
+            a: {
+              color: '#2563eb',
+              '&:hover': {
+                color: '#1d4ed8',
+              },
+            },
+          },
+        },
+      },
+    },
   },
   plugins: [
     require('@tailwindcss/typography'),
